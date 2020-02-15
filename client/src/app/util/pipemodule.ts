bdffc338a83ae39ba3910594862f7b3893dcebcf
Adérito Oliveira <aderito.nelson1@gmail.com>
2020-02-15 14:31:49 +0000

    Corrected the issue with the null in the search of the vehicles

--- client/src/app/util/pipemodule.ts
+++ client/src/app/util/pipemodule.ts
@@ -2,15 +2,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'filter'})
 export class FilterPipe implements PipeTransform {
-    transform(array: any[], value: string) {
+    transform(array, value) {
         if(value) {
             return array ? array.filter(item => 
-                //item.ADDRESS.toUpperCase().search(value.toUpperCase()) > -1) : [];
-                Object.keys(item).some(k => 
-                    item[k].toString().toLowerCase().includes(value.toLowerCase()))
-                ) : [];
-                //Object.keys(item).some(k => item[k].toLowerCase().includes(value.toLowerCase()))) : [];
-                //array.filter(x=>x.toLowerCase().indexOf(value.toLowerCase())!==-1)) : [];
+                Object.keys(item).some(k => item[k] != null && item[k].toString().toLowerCase().includes(value.toLowerCase()))) : [];
         }
         return array;
     }
--- client/src/app/vehicles/vehicles.component.ts
+++ client/src/app/vehicles/vehicles.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, ViewChild} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Vehicle } from './vehicles.model';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
 import { Router } from '@angular/router';
 
 import { VehicleService } from './vehicles.service';
@@ -20,12 +17,8 @@ import { GlobalCommunicationService } from '../globalcommunicationservice';
 export class VehiclesComponent implements OnInit {
   httpdata = null;
   filterValue = null;
-  displayedColumns: string[] = ['REGISTRATION_PLATE' ,'CAR_BRAND' ,'CAR_MODEL' ,'KMS_REGISTERED' ,'CYLINDER_CAPACITY' ,'REGISTRATION_DATE' ,'FABRICATION_DATE' ,'VEHICLE_IDENTIFICATION_NUMBER' ,'KILLOWATT' ,'HORSEPOWER' ,'FUEL' ,'CREATED_DATE' ,'MODIFIED_DATE'];
-  dataSource = new MatTableDataSource<Vehicle>(this.httpdata);
+  dataSource = <Vehicle> (this.httpdata);
 
-
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor(private router: Router,  private vehicleService: VehicleService,private globalCommunictionService: GlobalCommunicationService) { 
   }
@@ -47,28 +40,23 @@ export class VehiclesComponent implements OnInit {
 
   }
     
-  displaydata(data) {
+  /* displaydata(data) {
     this.httpdata = data;
     this.dataSource = new MatTableDataSource(data);
     console.log(this.httpdata)
-  }
+  } */
 
-  applyFilter(filterValue: string) {
+  /* applyFilter(filterValue: string) {
     this.filterValue = filterValue.trim().toLowerCase();
 
     console.log(filterValue);
 
-    this.dataSource.filter = filterValue;
-
-  }
+  } */
 
   getVehicles(): void {
     this.vehicleService.getVehicles().subscribe(data => {
       console.log(data); 
-      this.dataSource.data = data as any;
-
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+      this.dataSource = data
     });
   }
 
